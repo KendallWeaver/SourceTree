a822de14bdbfffc67df781fd3ab6154464afca06
unknown <10611465@CS514-10.ad.uvu.edu>
2015-09-02 09:00:55 -0600

    Variables And Functions

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿// File Prologue
+// Made by Kendall Weaver
+// Assignment: Variables and Functions
+
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+}
---
+++ New Unity Project/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour {
+
+	int theInt = 5;
+	const int TWO = 2;
+
+	// Use this for initialization
+	void Start () {
+		theInt = MultiplyByTwo (theInt);
+		Debug.Log (theInt);
+	}
+	
+	int MultiplyByTwo (int number) 
+	{
+		int ret;
+		ret = number * TWO;
+		return ret;
+	}
+}
