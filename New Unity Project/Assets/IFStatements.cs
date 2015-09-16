c8a492f70d8fccd7c4a5b511d885c488aa41f252
unknown <10611465@CS514-09.ad.uvu.edu>
2015-09-16 09:00:05 -0600

    IF Statements

---
+++ New Unity Project/Assets/IFStatements.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IFStatements : MonoBehaviour {
+	//Variables
+	public float coffeeTemp = 85.0f;
+	public float hotLimitTemp = 70.0f;
+	public float coldLimitTemp = 40.0f;
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		// Code that will deliver input when a key is pressed
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+		// Code that will change temp over time
+		coffeeTemp -= Time.deltaTime * 5f;
+	}
+
+	// New Function
+	public void TemperatureTest()
+	{
+		if (coffeeTemp >= hotLimitTemp)
+		{
+			Debug.Log ("Too hot");
+		}
+		else if (coffeeTemp <= coldLimitTemp)
+		{
+			Debug.Log ("Too cold");
+		}
+		else
+		{
+			Debug.Log ("Perfection");
+		}
+	}
+}
---
+++ New Unity Project/Assets/IFStatementsPractice.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IFStatementsPractice : MonoBehaviour {
+
+	public float coffeeTemp = 85.0f;
+	public float hotLimitTemp = 70.0f;
+	public float coldLimitTemp = 40.0f;
+
+	void Start () 
+	{
+	
+	}
+
+	void Update () 
+	{
+	//Code that indicates what button to press to see the the IF Statement Results
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+	// Code that will decrease the temperatures over time
+		coffeeTemp -= Time.deltaTime * 5f;
+	}
+
+	void TemperatureTest ()
+	{
+		if(coffeeTemp > hotLimitTemp)
+		{
+			Debug.Log ("This is too hot.");
+		}
+		else if(coffeeTemp < coldLimitTemp)
+		{
+			Debug.Log ("This is too cold.");
+		}
+		else
+		{
+			Debug.Log ("Perfection.");
+		}
+	}
+}
