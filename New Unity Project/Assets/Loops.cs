6d97d90a937a4440c87abdebb7f731d5572308c1
unknown <10611465@CS514-09.ad.uvu.edu>
2015-09-23 09:25:55 -0600

    Loops

---
+++ New Unity Project/Assets/Loops.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour {
+
+	int specialNumber = 3;
+	bool doWhileExp = false;
+
+	// Use this for initialization
+	void Start () 
+	{/*
+		while (specialNumber > 0) 
+		{
+			Debug.Log ("Cool");
+			specialNumber--;
+		}
+	*/
+		/*
+		do {
+			Debug.Log ("Runs at least once");
+		} while (doWhileExp == true); */
+
+		/*
+		for(int i = 0; i < specialNumber; i++)
+		{
+			Debug.Log (i);
+		}*/
+
+		string[] arrayOfStrings = new string[4];
+		arrayOfStrings[0] = "First String";
+		arrayOfStrings[1] = "Second String";
+		arrayOfStrings[2] = "Third String";
+		arrayOfStrings[3] = "Fourth String";
+
+		foreach(string array in arrayOfStrings)
+		{
+			Debug.Log (array);
+		}
+	}
+
+}
---
+++ New Unity Project/Assets/LoopsPractice.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopsPractice : MonoBehaviour {
+
+	int specialNumber = 3;
+	bool doWhileExp = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		/*
+		while(specialNumber > 0)
+		{
+			Debug.Log ("Number = " + specialNumber);
+			specialNumber--;
+		}*/
+
+		do
+		{
+			Debug.Log ("I'm going to run at least once regardless of the condition");
+		}while(doWhileExp == true);
+
+
+		for(int i = 0; i < specialNumber; i++)
+		{
+			Debug.Log("The number 'i' is below 3, therefore I will keep running");
+		}
+
+
+
+		string[] arrayOfStrings = new string[4];
+		arrayOfStrings[0] = "First String";
+		arrayOfStrings[1] = "Second String";
+		arrayOfStrings[2] = "Third String";
+		arrayOfStrings[3] = "Fourth String";
+
+		foreach(string thingy in arrayOfStrings)
+		{
+			Debug.Log(thingy);
+		}
+
+	}
+
+}
