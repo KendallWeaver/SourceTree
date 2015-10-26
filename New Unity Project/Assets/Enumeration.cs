b99ef85f360225bdb5848ecfbe41d7091261e5e4
unknown <10611465@CS514-09.ad.uvu.edu>
2015-10-26 09:36:38 -0600

    Enumerations

---
+++ New Unity Project/Assets/Enumeration.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enumeration : MonoBehaviour {
+
+	enum Testing {Value1, Value2, Value3};
+
+	void Start () 
+	{
+		Testing variableTesting;
+		variableTesting = Testing.Value1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	Testing Function (Testing test)
+	{
+		return test;
+	}
+}
---
+++ New Unity Project/Assets/EnumerationPractice.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnumerationPractice : MonoBehaviour {
+
+	//Enumerations (enum) is a special data type that has a subset of possible values
+
+	enum Direction {North, East, South, West};
+
+	// By default, North is 0, East is 1, and so on.
+	// These can be over written if needed.
+
+	// enum Direction {North = 1, East, South, West};
+	// enum Direction {North = 10, East = 11, South = 15, West = 18};
+
+	// The constants can be changed to any intergal type.
+	// enum Direction : short {};
+
+	// Use this for initialization
+	void Start () 
+	{
+		// You Can make variables of the enum Type
+		Direction myDirection;
+		// This can be treated like any other variable, but only the ones we set earlier
+		myDirection = Direction.North;
+	
+	}
+
+	void Update () 
+	{
+	
+	}
+
+	//Passing variables into a Function and can be returned
+	// Use the enum Name where you would a type
+	Direction ReverseDirection(Direction dir)
+	{
+		return dir;
+	}
+}
